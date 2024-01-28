27f85a432341a9e4f762d12adbcc63a4fc744860
isoca7 <isoca7@gmail.com>
2024-01-28 11:01:14 +0100

    añadir src

---
+++ src/main.js
@@ -0,0 +1,129 @@
+import confetti from 'canvas-confetti'
+let puntuacion = 0
+let carta_dada
+let mensajeResultado = document.getElementById('resultado')
+const botonDarCarta = document.getElementById('dame_carta')
+const botonParar = document.getElementById('parar')
+const containerBotones = document.getElementById('dar_reiniciar')
+const siguienteCarta = document.getElementById('siguiente_carta')
+
+const muestraPuntuacion = () => {
+  const elementoPuntuacion = document.getElementById('puntuacion')
+
+  if (elementoPuntuacion) {
+    elementoPuntuacion.innerHTML = `<h3>Tu puntuación es: <span>${puntuacion}</span></h3>`
+  }
+}
+
+document.addEventListener('DOMContentLoaded', muestraPuntuacion)
+
+const dameCarta = () => {
+  carta_dada = Math.floor(Math.random() * 10 + 1)
+  if (carta_dada > 7) {
+    carta_dada += 2
+  }
+}
+
+const sumarPuntuacion = () => {
+  if (carta_dada > 7) {
+    puntuacion += 0.5
+  } else {
+    puntuacion += carta_dada
+  }
+  if (puntuacion === 7.5) {
+    mensajeResultado.innerHTML = '¡ Lo has clavado! ¡Enhorabuena!'
+    mensajeResultado.setAttribute('class', 'rainbow')
+    confetti()
+    botonDarCarta.remove()
+    containerBotones.innerHTML = `<button type="button" id="reiniciar">Nueva partida 🎇</button>`
+  }
+}
+const muestraCarta = () => {
+  const imgCarta = document.getElementById('carta')
+  switch (carta_dada) {
+    case 1:
+      imgCarta.src =
+        'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg'
+      break
+    case 2:
+      imgCarta.src =
+        'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg'
+      break
+    case 3:
+      imgCarta.src =
+        'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg'
+      break
+    case 4:
+      imgCarta.src =
+        'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg'
+      break
+    case 5:
+      imgCarta.src =
+        'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg'
+      break
+    case 6:
+      imgCarta.src =
+        'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg'
+      break
+    case 7:
+      imgCarta.src =
+        'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg'
+      break
+    case 10:
+      imgCarta.src =
+        'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg'
+      break
+    case 11:
+      imgCarta.src =
+        'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg'
+      break
+    case 12:
+      imgCarta.src =
+        'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg'
+      break
+  }
+}
+
+const reiniciarJuego = () => {
+  location.reload()
+}
+const gestionarPartida = () => {
+  if (puntuacion <= 4.5) {
+    mensajeResultado.innerHTML = `<p>Has sido muy conservador</p>`
+    siguienteCarta.innerHTML = `<p>Hubieses sacado un ${carta_dada}</p>`
+  } else if (puntuacion >= 5 && puntuacion < 6) {
+    mensajeResultado.innerHTML = `<p>Te ha entrado el canguelo eh?</p>`
+    siguienteCarta.innerHTML = `<p>Hubieses sacado un ${carta_dada}</p>`
+  } else if (puntuacion >= 6 && puntuacion <= 7) {
+    mensajeResultado.innerHTML = `<p>Casi casi..</p>`
+    siguienteCarta.innerHTML = `<p>Hubieses sacado un ${carta_dada}</p>`
+  } else if (puntuacion === 7.5) {
+    mensajeResultado.innerHTML = '¡ Lo has clavado! ¡Enhorabuena!'
+    mensajeResultado.setAttribute('class', 'rainbow')
+    confetti()
+  }
+
+  botonDarCarta.remove()
+  containerBotones.innerHTML = `<button type="button" id="reiniciar">Nueva partida 🎇</button>`
+  const botonReiniciar = document.getElementById('reiniciar')
+  botonReiniciar.addEventListener('click', reiniciarJuego)
+}
+
+const gameOver = () => {
+  if (puntuacion > 7.5) {
+    mensajeResultado.innerHTML = `<p style='color:black; font-size: 3em; text-shadow: 0 0 20px #fff, 0 0 30px #fff, 0 0 50px #fff, 0 0 60px #fff, 0 0 70px #fff'>Game over!</p>`
+    botonDarCarta.remove()
+    containerBotones.innerHTML = `<button type="button" id="reiniciar">Nueva partida 🎇</button>`
+  }
+  const botonReiniciar = document.getElementById('reiniciar')
+  botonReiniciar.addEventListener('click', reiniciarJuego)
+}
+
+botonDarCarta.addEventListener('click', dameCarta)
+botonDarCarta.addEventListener('click', sumarPuntuacion)
+botonDarCarta.addEventListener('click', muestraPuntuacion)
+botonDarCarta.addEventListener('click', muestraCarta)
+botonDarCarta.addEventListener('click', gameOver)
+botonParar.addEventListener('click', dameCarta)
+botonParar.addEventListener('click', gestionarPartida)
+botonParar.addEventListener('click', gameOver)
